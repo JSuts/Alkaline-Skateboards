57269e26ba336caba33bf03fdd45fc4237c285ab
Jake <jsutter449@west-mec.org>
2019-06-13 00:40:23 -0400

    Started Projects Page

--- html/js/home.js
+++ html/js/home.js
@@ -1,8 +1,3 @@
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 function fadeOut() {
   document.getElementById('scrollText').style.opacity = '0';
 }
