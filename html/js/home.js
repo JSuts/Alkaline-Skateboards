aaa1b58860c6095235171cc0147a003c4f73b0cb
Jake <jsutter449@west-mec.org>
2019-06-12 23:09:12 -0400

    Made subtle changes asked for.
    Bottom font changed. Logo centered (Old code for left aligned logo still in file). Added Scroll and left/right rectangles.

--- html/js/home.js
+++ html/js/home.js
@@ -2,3 +2,22 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function fadeOut() {
+  document.getElementById('scrollText').style.opacity = '0';
+}
+
+function fadeIn() {
+  document.getElementById('scrollText').style.opacity = '1';
+}
+
+
+window.addEventListener('scroll', function() {
+  var currentScrollPos = window.pageYOffset;
+  var topImageHeight = document.getElementById('topImage').height;
+  if (currentScrollPos > topImageHeight) {
+    fadeOut();
+  } else {
+    fadeIn();
+  }
+});
