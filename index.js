50a5d74d4d786de393e116008f2f50a405e74b06
Jake <jakesutter1206@gmail.com>
2019-07-23 14:08:57 -0400

    Updated Server
    Changed port to 3000 for the server and the middleware to make the files work.

--- index.js
+++ index.js
@@ -3,21 +3,13 @@ const path = require('path');
 const fs = require('fs');
 
 var app = express();
-var port = 8080;
+var port = 3000;
 
 app.set('view engine', 'html');
-app.use( '/' , express.static(path.join(__dirname ,'..' ,'public')));
-
-
-app.get('/test', function(req, res, next) {
-    var html = fs.readFileSync('./html/home.html', 'utf8')
-    // res.render('test', { html: html })
-    res.send(html);
-})
+app.use( '/' , express.static(path.join(__dirname ,'/html')));
 
 app.use('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/html/home.html'));
-  // res.send("Hello");
 })
 
 app.listen(port, () => {
