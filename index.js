0d10127f11da115db9433af0c4863837e7994b22
JSuts <jakesutter1206@gmail.com>
2020-09-25 16:20:05 -0400

    Full send. Trying auto pull (for discord bot).

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const fs = require('fs');
 const https = require('https');
 
+const { exec } = require("child_process");
+
 var app = express();
 var port = 3000;
 
@@ -11,6 +13,23 @@ app.use( '/' , express.static(path.join(__dirname ,'/html')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/html/home.html'));
+})
+
+app.get('/git-pull', (req, res) => {
+  console.log('Pulling (probably)');
+  console.log(req);
+  exec('cd /home/ubuntu/Discord-Bot && git pull', (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  res.send("Hey");
 })
 
 app.listen(port, () => {
